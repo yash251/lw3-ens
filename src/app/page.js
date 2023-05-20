f9189924f3beb598e0cd50056b09ed63b915a344
Yash Jagtap <jagtapyash2512@gmail.com>
2023-05-20 23:55:19 +0530

    add imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,8 @@
-import Image from 'next/image'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Web3Modal from "web3modal";
+import { ethers, providers } from "ethers";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   return (
