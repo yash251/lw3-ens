64d70c808ba026c85a928c7aee640ffee1ee5541
Yash Jagtap <jagtapyash2512@gmail.com>
2023-05-22 22:15:43 +0530

    getProviderOrSigner

--- src/app/page.js
+++ src/app/page.js
@@ -29,6 +29,26 @@ export default function Home() {
     }
   };
 
+  const getProviderOrSigner = async () => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Goerli network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Change the network to Goerli");
+      throw new Error("Change network to Goerli");
+    }
+    const signer = web3Provider.getSigner();
+    // Get the address associated to the signer which is connected to  MetaMask
+    const address = await signer.getAddress();
+    // Calls the function to set the ENS or Address
+    await setENSOrAddress(address, web3Provider);
+    return signer;
+  };
+
   return (
     <div>
       
