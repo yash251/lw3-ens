d0fdc9930157bc1de1aa4d5f3d4236110f733b3a
Yash Jagtap <jagtapyash2512@gmail.com>
2023-05-23 23:57:51 +0530

    use client

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import { ethers, providers } from "ethers";
 import { useEffect, useRef, useState } from "react";
+"use client";
 
 export default function Home() {
   // walletConnected keep track of whether the user's wallet is connected or not
@@ -88,7 +89,29 @@ export default function Home() {
 
   return (
     <div>
-      
+      <Head>
+        <title>ENS Dapp</title>
+        <meta name="description" content="ENS-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>
+            Welcome to YashWeb3 Punks {ens ? ens : address}!
+          </h1>
+          <div className={styles.description}>
+            It&#39;s an NFT collection for YashWeb3 Punks.
+          </div>
+          {renderButton()}
+        </div>
+        <div>
+          <img className={styles.image} src="./learnweb3punks.png" />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Made with &#10084; by YashWeb3 Punks
+      </footer>
     </div>
   )
 }
